82934085812e4d5adc6d4abe8882872f13df1aa4
Clive Patrick <clive.m.patrick@gmail.com>
2022-06-29 14:52:24 +1000

    Item placement will ignore flow areas

--- Assets/Scripts/ItemPlacement.cs
+++ Assets/Scripts/ItemPlacement.cs
@@ -32,7 +32,7 @@ public class ItemPlacement : MonoBehaviour
 
     private void Update()
     {
-        var overlappingCollider = Physics2D.OverlapCircle(transform.position, item.size);
+        var overlappingCollider = Physics2D.OverlapCircle(transform.position, item.size, ~LayerMask.GetMask("FlowArea"));
         canPlace = overlappingCollider == null;
 
         spriteRenderer.enabled = true;
