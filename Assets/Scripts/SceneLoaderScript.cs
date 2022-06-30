50d73616218e29611d9349513e1601ba363d82e7
Evita Liu <evitaliu2001@gmail.com>
2022-06-30 14:20:15 +1000

    main menu now goes to level select

--- Assets/Scripts/SceneLoaderScript.cs
+++ Assets/Scripts/SceneLoaderScript.cs
@@ -17,9 +17,35 @@ public class SceneLoaderScript : MonoBehaviour
         
     }
 
-    public void loadLevel()
+    public void loadTestLevel()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void loadLvlSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+    public void loadLvl1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void loadLvl2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void loadLvl3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+    public void loadLvl4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+    public void loadLvl5()
+    {
+        SceneManager.LoadScene("Level5");
     }
 
     public void quitGame()
